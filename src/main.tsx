d2cb90f758efb041bcf1f091635c052202c382f8
Nathaniel Meyer <nathaniel.lorne@gmail.com>
2025-04-28 14:22:49 -0600

    maybe this will work

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,21 @@ import Callback from "./Callback.tsx";
 import "./index.css";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
 
 Amplify.configure(outputs);
 
+// Define the router
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="callback" element={<Callback />} />
+    </Route>
+  )
+);
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-    <Callback />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
